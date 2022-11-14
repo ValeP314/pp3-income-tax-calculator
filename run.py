87ccc28791ebbb40a570ce5f2fb54056b95cef67
ValeP314 <valeriaparisani@hotmail.com>
2022-11-14 15:51:39 +0000

    Design main structure and add general queries to be asked in order to use the calculator

--- run.py
+++ run.py
@@ -1,3 +1,37 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# This tax income calculator will ask some general information
+# in order to calculate the deductions applicable to our gross salary,
+# and return annual and monthly net salary.
+
+
+#print("What is your name?")
+#full_name = input("Enter your full name here:\n")
+print("Please type in your annual gross salary:")
+gross_salary = int(input("Enter your annual gross salary here:\n"))
+#print("Do you have any dependants?")
+#dependants = input("True or False\n")
+print("Do you pay any pension?")
+pension = int(input("Enter your annual gross salary here:\n"))
+    
+taxable_salary = (gross_salary - pension)
+
+
+def taxes():
+    if taxable_salary < 36800:
+        tax = taxable_salary * 0.20
+    
+    else:
+        excess = taxable_salary - 36800
+        first_band_taxes = 36800 * 0.20
+        tax = first_band_taxes + (excess * 0.40)
+    
+    print(f"You pay {tax} €")
+
+
+def main():
+    """
+    Run all program functions
+    """
+    taxes()
+    
+
+main()
